762a127f9abefbb17c4955808da1be5ba1d987f2
mauro <maurop123@gmail.com>
2017-09-26 11:00:01 -0400

    test component with handlebars

---
+++ Btn.vue
@@ -0,0 +1,15 @@
+<template>
+      <v-btn fixed fab bottom right
+        class="red" dark
+        @click="click"
+        >
+        <slot />{{atom}}
+      </v-btn>
+</template>
+
+<script>
+export default {
+  props: ['click'],
+}
+
+</script>
